ec957e9779199a71140bb9dd6b76f76fee44f297
Vas Bakhmut <mizaracnc@gmail.com>
2024-01-26 19:39:35 +1100

    last, not finished cos design, PC only

---
+++ slidersecond.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlidesBounds: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
